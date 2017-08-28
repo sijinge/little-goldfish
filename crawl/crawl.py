f351e6dbae5ea16ab8677830a853de8d272e8ca1
sijinge <523530260@qq.com>
2017-08-28 15:08:52 +0800

    初次提交

---
+++ crawl/crawl.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+'''
+   作者：sijinge
+   时间：2017-08-28
+   功能：
+'''
+
+class Crawl():
+    #程序启动
+    def start(self):
+        pass
+
+    #程序停止
+    def stop(self):
+        pass
+
+    def
